571a82e8ea1a7b6296f23f7aaafd2840f3157453
Lucas Zosi <zosi.lucas@gmail.com>
2023-01-26 10:04:27 -0300

    Fix path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,7 +18,7 @@ function NavBar({ brand }) {
 
     return (
         <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-            <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <a href="/PreEntrega1-Zosi/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                 { brand }
             </a>
 
