de98207f28f8d8f17f97db4e96885443ba1521bf
Lucas Zosi <zosi.lucas@gmail.com>
2023-01-26 09:04:35 -0300

    Primera entrega

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <NavBar brand="ZetaEle" />
+      <ItemListContainer greeting="Hola, cliente" />
+    </div>
+  )
+}
+
+export default App
