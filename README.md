220c33cb2075f421fa3ce3587815f4ed6aac314e
Lucas Zosi <zosi.lucas@gmail.com>
2023-01-26 09:48:20 -0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# PreEntrega1-Zosi
-[CoderHouse] PreEntrega 1
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/PreEntrega1-Zosi/',
-  plugins: [react()],
-})
